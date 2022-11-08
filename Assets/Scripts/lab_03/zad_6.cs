908204695a0cc48e674c1f930cc2509b7cb5f55e
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-11-08 11:25:09 +0100

    zad 6

---
+++ Assets/Scripts/lab_03/zad_6.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad_6 : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public float yVelocity = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.SmoothDamp(transform.position.y, target.position.y, ref yVelocity, smoothTime);
+        transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+    }
+}
