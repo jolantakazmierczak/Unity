20088a1699572b5f4dadd8b6467b491ad8f48b1c
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-11-10 10:37:56 +0100

    lab 5

--- Assets/Scripts/lab_03/zad_6.cs
+++ Assets/Scripts/lab_03/zad_6.cs
@@ -1,4 +1,5 @@
-using System.Collections;
+/*
+ * using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,3 +16,5 @@ public class zad_6 : MonoBehaviour
         transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
     }
 }
+
+*/
